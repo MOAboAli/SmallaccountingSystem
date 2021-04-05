84291cb36c59b6e5f4001a3b8aaa5e1d7044ce43
Mohamed Aboali <mohamedaboali_aboali@hotmail.com>
2021-04-05 21:28:02 +0200

    Add files via upload

---
+++ Account/Login.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using BsolutionWebApp.Models;
+
+namespace BsolutionWebApp.Account
+{
+    public partial class Login : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RegisterHyperLink.NavigateUrl = "Register";
+            // Enable this once you have account confirmation enabled for password reset functionality
+            //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
+            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
+            var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+            if (!String.IsNullOrEmpty(returnUrl))
+            {
+                RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+            }
+        }
+
+        protected void LogIn(object sender, EventArgs e)
+        {
+            using (BsolutionDBDataContext DB = new BsolutionDBDataContext())
+            {
+                //var button = DB.Users.Where(a => a.Username.Equals(txtUsername.Text) && a.password.Equals(txtPassword.Text));
+                //if (button.Count() > 0)
+                //{
+                //    var user = button.First();
+                //    //Labelusername.Text = user.Name;
+                //    ////Session["UserID"] = user.ID;
+                //    //Labeluserid.Text = Convert.ToString(user.ID);
+
+                //}
+                //else
+                //{
+                //    //Labeluserid.Text = "0";
+                //    //// Session["UserID"] = 0;
+                //    //Labelusername.Text = "None, Please Login";
+                //    //Labelstatuslogin.Text = "LoginFailed";
+                //}
+            }
+
+
+            Response.Redirect("~/Pages/AdminPages/Home.aspx?user=");
+
+            //if (IsValid)
+            //{
+            //    // Validate the user password
+            //    var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            //    var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+            //    // This doen't count login failures towards account lockout
+            //    // To enable password failures to trigger lockout, change to shouldLockout: true
+            //    var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
+
+            //    switch (result)
+            //    {
+            //        case SignInStatus.Success:
+            //            IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            //            break;
+            //        case SignInStatus.LockedOut:
+            //            Response.Redirect("/Account/Lockout");
+            //            break;
+            //        case SignInStatus.RequiresVerification:
+            //            Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
+            //                                            Request.QueryString["ReturnUrl"],
+            //                                            RememberMe.Checked),
+            //                              true);
+            //            break;
+            //        case SignInStatus.Failure:
+            //        default:
+            //            FailureText.Text = "Invalid login attempt";
+            //            ErrorMessage.Visible = true;
+            //            break;
+            //    }
+            //}
+        }
+    }
+}
